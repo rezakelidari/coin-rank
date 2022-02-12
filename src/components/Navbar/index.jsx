eed6a4c7476791881a9a596e47f0365c428750e4
Reza Kelidari <reza.phoenix.1382@gmail.com>
2022-02-12 16:23:19 +0330

    Making Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.scss";
+
+export default function Navbar() {
+  return (
+    <nav className="navbarMain">
+      <h1>Coin Rank</h1>
+    </nav>
+  );
+}
